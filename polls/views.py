39ec9c8552080279f8cd760f91da991333a299d6
TAKAHASHI Shuuji <shuuji3@gmail.com>
2023-08-27 04:53:26 +0900

    feat: finish part 3 view and template

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,24 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+
+from polls.models import Question
 
 
-def index(request, year=''):
-    return HttpResponse(f"<h1>hello, world. you're at the polls index. Year: {year}!</h1>")
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    return render(request, 'polls/index.html', {'latest_question_list': latest_question_list})
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+    # return HttpResponse(f"You're looking at question {question_id}.")
+
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the results of question {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}")
